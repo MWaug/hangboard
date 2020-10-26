7ac189dcf4a59a6bbb586181f327cf3ba72d8f03
Max Waugaman <mwaugaman@galois.com>
2020-10-25 23:21:34 -0400

    Updated code with max and average weight. Working on battery powered ESP8266 prototype

--- websocket_test/webpage.h
+++ websocket_test/webpage.h
@@ -2,24 +2,27 @@
 // formatted as a string literal!
 char webpage[] PROGMEM = R"=====(
 <html>
+<div>
+  <h1 id="currentHang">Current Hang: None</h1>
+  <h1 id="lastHang">Last Hang: None</h1>
+  <h1 id="maxWeight">Max Weight: None</h1>
+  <h1 id="aveWeight">Ave Weight: None</h1>
+</div>
 <!-- Adding a data chart using Chart.js -->
 <head>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js'></script>
 </head>
 <body onload="javascript:init()">
-<!-- Adding a slider for controlling data rate -->
+<!-- Adding a slider for controlling scale calibration slope -->
 <div>
-  <input type="range" min="2" max="100" value="50" id="dataRateSlider" oninput="sendDataRate()" />
-  <label for="dataRateSlider" id="dataRateLabel">Period: 50ms</label>
+  <input type="range" min="9000" max="18000" value="15600" id="calibSlider" oninput="sendCalib()" />
+  <label for="calibSlider" id="calibSliderLabel">Scale Units Per LB: 15600</label>
 </div>
 <hr />
 <div>
   <canvas id="line-chart" width="800" height="450"></canvas>
 </div>
-<div>
-  <h1 id="currentHang">Current Hang: None</h1>
-  <h1 id="lastHang">Last Hang: None</h1>
-</div>
+
 <!-- Adding a websocket to the client (webpage) -->
 <script>
   var webSocket, dataPlot;
@@ -46,21 +49,33 @@ char webpage[] PROGMEM = R"=====(
           borderColor: "#3e95cd",
           fill: false
         }]
+      },
+      options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    suggestedMin: -10,
+                    suggestedMax: 250
+                }
+            }]
+        }
       }
     });
     webSocket.onmessage = function(event) {
       var data = JSON.parse(event.data);
       var today = new Date();
-      var t = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      var t = today.getMinutes() + ":" + today.getSeconds();
       addData(t, data.value);
       document.getElementById("currentHang").innerHTML = "Current Hang: " + data.ch + "s";
       document.getElementById("lastHang").innerHTML = "Last Hang: " + data.lh + "s";
+      document.getElementById("maxWeight").innerHTML = "Max Weight: " + data.max + "lb";
+      document.getElementById("aveWeight").innerHTML = "Ave Weight: " + data.ave + "lb";
     }
   }
-  function sendDataRate(){
-    var dataRate = document.getElementById("dataRateSlider").value;
-    webSocket.send(dataRate);
-    document.getElementById("dataRateLabel").innerHTML = "Period: " + dataRate + "ms";
+  function sendCalib(){
+    var calib = document.getElementById("calibSlider").value;
+    webSocket.send(calib);
+    document.getElementById("calibSliderLabel").innerHTML = "Scale Units Per LB: " + calib;
   }
 </script>
 </body>
--- websocket_test/websocket_test.ino
+++ websocket_test/websocket_test.ino
@@ -8,17 +8,23 @@
 
 // Reference: https://github.com/acrobotic/Ai_Demos_ESP8266/tree/master/bmp180_gui
 
-// Sensor variables and functions
+/* 
+ *  Sensor variables
+ */
 Q2HX711 hx711(D5,D6);
-long offset = 7784000; // Scale at zero lbs
-long calib_180lbs = 4500000;
-long scale_unit_per_lb = 13500;
+long offset = 7785000; // Scale at zero lbs
+long scale_unit_per_lb = 15600;
 unsigned long StartHang = 0;
 unsigned long EndHang = 0;
-long hang_threshold = 64;
+long hang_threshold = 15;
 float last_hang_secs = -1;
 float cur_hang_secs = -1;
 float cur_weight = 0;
+float defaultDataRate_ms = 50;
+float maxWeight = 0;
+float integratedWeight = 0;
+float aveWeight = 0;
+unsigned long lastMeasuredTime = 0;
 
 enum state {
   IN_HANG,
@@ -27,6 +33,9 @@ enum state {
 
 enum state cur_state = ON_GROUND;
 
+/* 
+ *  Webserver variables
+ */
 // Connecting to the Internet
 const char* ssid = SECRET_SSID; 
 const char* password = SECRET_PASSWORD;
@@ -41,7 +50,9 @@ ESP8266WebServer server;
 // Adding a websocket to the server
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-// Internet Connectivity
+/* 
+ *  Internet Connectivity
+ */
 void connect_to_internet() {
   Serial.print("Connecting to ");
   Serial.println(ssid); 
@@ -72,9 +83,10 @@ void setup_server() {
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
   // Do something with the data from the client
   if(type == WStype_TEXT){
-    float dataRate = (float) atof((const char *) &payload[0]);
-    timer.detach();
-    timer.attach_ms(dataRate, getData);
+    float scale_unit_per_lb_float = (float) atof((const char *) &payload[0]);
+    scale_unit_per_lb = (long) scale_unit_per_lb_float;
+    // timer.detach();
+    // timer.attach_ms(dataRate, getData);
   }
 }
 
@@ -82,28 +94,41 @@ void getData() {
   get_data = true;
 }
 
-void sendData(float w, float ch, float lh) {
+void sendData(float w, float ch, float lh, float m, float ave) {
   String json = "{\"value\":";
     json += String(w, 2);
-    json += ",\"lh\":";
-    json += String(ch, 2);
     json += ",\"ch\":";
+    json += String(ch, 2);
+    json += ",\"lh\":";
     json += String(lh, 2);
+    json += ",\"max\":";
+    json += String(m, 2);
+    json += ",\"ave\":";
+    json += String(ave, 2);
     json += "}";
   webSocket.broadcastTXT(json.c_str(), json.length());
 }
 
-// Digital scale
+/* 
+ *  Scale update logic
+ */
 float update_scale() {
-  // Serial.println("Reading from scale");
   float weight = (offset - hx711.read()) / scale_unit_per_lb;
-  // Serial.println("Read from scale");
   if( (cur_state == ON_GROUND) && (weight > hang_threshold) ) {
       StartHang = millis();
       Serial.println("Hanging!");
       cur_state = IN_HANG;
-      cur_hang_secs = (millis() - StartHang) / 1000.0;
+      maxWeight = 0;
+      integratedWeight = 0;
+      aveWeight = 0;
     }
+  // Calculate hang statistics
+  if ( cur_state == IN_HANG ) {
+    if( weight > maxWeight ) { maxWeight = weight; }
+    integratedWeight = integratedWeight + ( weight * (millis() - lastMeasuredTime) );
+    aveWeight = integratedWeight / (millis() - StartHang);
+    cur_hang_secs = (millis() - StartHang) / 1000.0;
+  }
   if( (cur_state == IN_HANG) && weight < hang_threshold - 5) {
       EndHang = millis();
       unsigned long hang_dur = EndHang - StartHang;
@@ -114,25 +139,29 @@ float update_scale() {
       cur_hang_secs = 0;
       cur_state = ON_GROUND;
   }
+  lastMeasuredTime = millis();
   return weight;
+}
+
+void zero_out_scale(){
+   long zero_value = (offset - hx711.read());
+   offset = offset - zero_value;
 }
 
 void setup() {
   Serial.begin(115200);
   connect_to_internet();
   setup_server();
-  timer.attach_ms(500, getData);
+  zero_out_scale();
+  timer.attach_ms(defaultDataRate_ms, getData);
 }
 
 void loop() {
-  // Websocket 
   webSocket.loop();
   server.handleClient();
   if(get_data) {
-    sendData(cur_weight, cur_hang_secs, last_hang_secs);
+    sendData(cur_weight, cur_hang_secs, last_hang_secs, maxWeight, aveWeight);
     get_data = false;
   }
-  // Serial.println("Getting curr weight");
   cur_weight = update_scale();
-  // Serial.println(cur_weight);
 }
