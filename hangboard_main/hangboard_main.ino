d7b80f2b97ed53b10fc7edb8ff8030b4dff17e30
Max Waugaman <mwaugaman@galois.com>
2021-02-21 08:41:40 -0500

    Updated default weight and person selection

--- hangboard_main/hangboard_main.ino
+++ hangboard_main/hangboard_main.ino
@@ -16,7 +16,7 @@ float aveWeight = 0;
 
 // Application data sent from webpage
 const char* person = "Max";
-long calib = 15600;
+long calib = 17098;
 const char* rung = "middle-edge";
 const char* note = "";
 int dataLogEnable = 1;
