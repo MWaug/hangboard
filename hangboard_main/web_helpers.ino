44874a377cee1bab802ff53c0712919e67c4206c
Max Waugaman <mwaugaman@galois.com>
2021-03-26 12:39:17 -0400

    Renamed websocket_helpers.ino

--- hangboard_main/web_helpers.ino
+++ hangboard_main/web_helpers.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include "secret.h"
 #include "webpage.h"
 #include "ArduinoJson.h"
@@ -24,6 +27,57 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 // See https://bblanchon.github.io/ArduinoJson/assistant/
 StaticJsonDocument<200> jsonDoc;
 
+/*
+ * OTA functions
+ */
+void OTA_setup() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+}
+
 /* 
  *  Internet Connectivity
  */
@@ -42,6 +96,7 @@ void connect_to_internet() {
 }
 
 void setup_server( void (*eventHandler)(uint8_t, WStype_t, uint8_t *, size_t) ) {
+  OTA_setup();
   Serial.println("");
   server.begin();
   Serial.println("Server started");
@@ -56,6 +111,7 @@ void setup_server( void (*eventHandler)(uint8_t, WStype_t, uint8_t *, size_t) ) 
 
 
 void tickServer() {
+  ArduinoOTA.handle();
   webSocket.loop();
   server.handleClient();
 }
